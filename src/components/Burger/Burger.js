8bd2f1c4080e6f4bbfc59f54df864ab4eb2d5a8d
Adam Bien <adam.bien94@gmail.com>
2019-10-03 18:39:23 +0200

    burger builder / ingridients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  let transformedIgredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIgredients.length === 0) {
+    transformedIgredients = <p>Pealse start adding Ingredients!</p>;
+  }
+  console.log(transformedIgredients);
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIgredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
